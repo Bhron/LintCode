5ebc31524b85352083e3bb668b34aec3131f9d1b
Jianhao Guo <gjhbyron@gmail.com>
2016-04-23 13:01:34 -0700

    Add C++ solution for problem 457.Classical Binary Search.

---
+++ Binary_Search/457_Classical_Binary_Search.cpp
@@ -0,0 +1,1 @@
+../Easy/457_Classical_Binary_Search.cpp
---
+++ Easy/457_Classical_Binary_Search.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    /**
+     * @param A an integer array sorted in ascending order
+     * @param target an integer
+     * @return an integer
+     */
+    int findPosition(vector<int>& A, int target) {
+        if (A.empty()) {
+            return -1;
+        }
+
+        int start = 0, end = A.size() - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+
+            if (A[mid] == target) {
+                return mid;
+            } else if (A[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (A[start] == target) {
+            return start;
+        }
+        if (A[end] == target) {
+            return end;
+        }
+        return -1;
+    }
+};
