70f8fa451a5a9af5c711536763d4764b6a929d3f
Jianhao Guo <gjhbyron@gmail.com>
2016-04-25 00:11:30 -0700

    Add another solution for problem 75.Find Peak Element.

--- Medium/75_Find_Peak_Element.cpp
+++ Medium/75_Find_Peak_Element.cpp
@@ -23,3 +23,26 @@ public:
         return high;
     }
 };
+
+class Solution {
+public:
+    /**
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    int findPeak(vector<int> A) {
+        int low = 0, high = A.size() - 1;
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            if (A[mid - 1] < A[mid] && A[mid] > A[mid + 1]) {
+                return mid;
+            } else if (A[mid - 1] > A[mid] && A[mid] < A[mid + 1]) {
+                high = mid;
+            } else if (A[mid - 1] < A[mid] && A[mid] < A[mid + 1]) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+    }
+};
