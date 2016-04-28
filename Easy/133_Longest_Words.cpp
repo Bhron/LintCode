9c2260f2b8f904669f5a6bdca6fae496ac2b40b0
Jianhao Guo <gjhbyron@gmail.com>
2016-04-28 01:03:24 -0700

    Add another solution for problem 133.Longest Words.

--- Easy/133_Longest_Words.cpp
+++ Easy/133_Longest_Words.cpp
@@ -1,3 +1,34 @@
+// Two Pass
+class Solution {
+public:
+    /**
+     * @param dictionary: a vector of strings
+     * @return: a vector of strings
+     */
+    vector<string> longestWords(vector<string> &dictionary) {
+        vector<string> words;
+
+        if (dictionary.empty()) {
+            return words;
+        }
+
+        int max_len = 0;
+        for (int i = 0; i < dictionary.size(); i++) {
+            if (dictionary[i].length() > max_len) {
+                max_len = dictionary[i].length();
+            }
+        }
+
+        for (int i = 0; i < dictionary.size(); i++) {
+            if (dictionary[i].length() == max_len) {
+                words.push_back(dictionary[i]);
+            }
+        }
+
+        return words;
+    }
+};
+
 // One Pass
 class Solution {
 public:
