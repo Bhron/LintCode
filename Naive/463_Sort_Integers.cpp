7f772b7c7a1d5507c11988b5e38013705febe62c
Jianhao Guo <gjhbyron@gmail.com>
2016-06-28 00:34:33 -0400

    Add another solution for the problem 463.Sort Integers.

--- Naive/463_Sort_Integers.cpp
+++ Naive/463_Sort_Integers.cpp
@@ -25,3 +25,31 @@ private:
         b = tmp;
     }
 };
+
+// Selection Sort
+class Solution {
+public:
+    /**
+     * @param A an integer array
+     * @return void
+     */
+    void sortIntegers(vector<int>& A) {
+        if (A.empty()) {
+            return;
+        }
+
+        for (int i = 0; i < A.size() - 1; i++) {
+            for (int j = 0; j < A.size() - 1 - i; j++) {
+                if (A[j] > A[A.size() - 1 - i]) {
+                    swap(A[j], A[A.size() - 1 - i]);
+                }
+            }
+        }
+    }
+private:
+    void swap(int& a, int& b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+};
