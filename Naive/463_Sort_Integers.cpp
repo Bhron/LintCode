78fbafb27dd9efe8ace751c62257e95ba198eb54
Jianhao Guo <gjhbyron@gmail.com>
2016-06-29 09:11:54 -0400

    Add another solution for problem 463.Sort Integers.

--- Naive/463_Sort_Integers.cpp
+++ Naive/463_Sort_Integers.cpp
@@ -1,4 +1,4 @@
-// Bubble Sort
+// Bubble Sort, Time Complexity O(n^2)
 class Solution {
 public:
     /**
@@ -26,7 +26,7 @@ private:
     }
 };
 
-// Selection Sort
+// Selection Sort, Time Complexity O(n^2)
 class Solution {
 public:
     /**
@@ -44,6 +44,34 @@ public:
                     swap(A[j], A[A.size() - 1 - i]);
                 }
             }
+        }
+    }
+private:
+    void swap(int& a, int& b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+};
+
+// Insertion Sort, Time Complexity O(n^2)
+class Solution {
+public:
+    /**
+     * @param A an integer array
+     * @return void
+     */
+    void sortIntegers(vector<int>& A) {
+        if (A.empty()) {
+            return;
+        }
+
+        for (int i = 1; i < A.size(); i++) {
+            int val = A[i], j;
+            for (j = i - 1; j >= 0 && A[j] > val; j--) {
+                A[j + 1] = A[j];
+            }
+            A[j + 1] = val;
         }
     }
 private:
